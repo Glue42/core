efe2312cd25e2cd08c1ffa9da8474aad45efc75b
Kalin Kostov <k.kostov.eu@gmail.com>
2020-03-10 22:58:10 +0200

    added first completely mocked server for testing
    Signed-off-by: flashd2n <k.kostov.eu@gmail.com>

--- packages/core-dev-server/test/first.spec.ts
+++ packages/core-dev-server/test/first.spec.ts
@@ -1,8 +1,35 @@
 import { expect } from "chai";
+import mockery from "mockery";
 import "mocha";
+import { CoreDevServer } from "../src/server";
+import { httpMock, expressMock, httpProxyMock, morganMock, requestMock, parserMock } from "./mocks";
 
-describe("first test suite", function() {
-    it("first test", () => {
+describe("first test suite", function () {
+    this.timeout(10000);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let Server: any;
+
+    before(() => {
+        mockery.enable();
+        mockery.registerMock("http", httpMock);
+        mockery.registerMock("express", expressMock);
+        mockery.registerMock("morgan", morganMock);
+        mockery.registerMock("request", requestMock);
+        mockery.registerMock("concat-stream", expressMock);
+        mockery.registerMock("http-proxy", httpProxyMock);
+        Server = require("../src/server.ts").CoreDevServer;
+    });
+
+    after(() => {
+        mockery.deregisterAll();
+        mockery.disable();
+    });
+
+    it("first test", async () => {
+        const server: CoreDevServer = new Server(parserMock);
+
+        await server.setup();
+        console.log(server);
         expect(1).to.eql(1);
     });
 });
