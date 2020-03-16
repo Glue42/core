0f1cd80e4360b364ebe64f67f72481fe71561bed
3lmo <emi.petkov@gmail.com>
2020-03-16 15:05:20 +0200

    refactor(rest-server): targetDir setup
    Signed-off-by: 3lmo <emi.petkov@gmail.com>

--- tutorial/rest-server/server.js
+++ tutorial/rest-server/server.js
@@ -5,11 +5,9 @@ const bodyParser = require('body-parser');
 const clients = require('./data/clients');
 const portfolio = require('./data/portfolio');
 
-const API_JSON_PATH = './data/';
-const API_URL_PREFIX = 'api';
-// const IS_DEV = process.env.NODE_ENV === 'development';
-const TARGET_DIR = process.argv0 || './'
-console.log(process.argv0)
+// const API_JSON_PATH = './data/';
+const API_URL_PREFIX = '/api';
+const TARGET_DIR = process.argv[2] || './';
 
 const app = express();
 
@@ -17,11 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get(`${API_URL_PREFIX}/clients`, (req, res) => {
-    res.sendFile(path.resolve(path.join(API_JSON_PATH, 'clients.json')));
+    res.json(clients);
 });
 
 app.get(`${API_URL_PREFIX}/portfolio`, (req, res) => {
-    res.sendFile(path.resolve(path.join(API_JSON_PATH, 'portfolios.json')));
+    res.json(portfolio);
 });
 
 app.get(`${API_URL_PREFIX}/portfolio/:userId`, (req, res) => {
@@ -37,7 +35,7 @@ app.get(`${API_URL_PREFIX}/portfolio/:userId`, (req, res) => {
 app.use(function(req, res, next) {
     if (path.basename(req.path).length > 0 && fs.existsSync(path.join(TARGET_DIR, req.path))) {
         res.sendFile(path.resolve(path.join(TARGET_DIR, req.path)));
-    } else if (path.extname(req.path).length > 0) {
+    } else if (req.path.startsWith(API_URL_PREFIX)) {
         next();
     } else {
         res.sendFile(path.resolve(TARGET_DIR, 'index.html'));
@@ -50,5 +48,5 @@ app.listen(port, function(error) {
     if (error) {
         throw error;
     }
-    console.log('Express server listening on port', port); // tslint:disable-line no-console
+    console.log(`Serving directory ${path.resolve(process.argv[2])} on port ${port}`);
 });
