dfcdf839f1227a615437885868372a22fff6ca09
Kalin Kostov <k.kostov.eu@gmail.com>
2020-07-22 10:42:43 +0300

    simplified promise wrapper
    Signed-off-by: Kalin Kostov <k.kostov.eu@gmail.com>

--- packages/web/src/shared/promise-plus.ts
+++ packages/web/src/shared/promise-plus.ts
@@ -1,13 +1,8 @@
 export const promisePlus = <T>(promise: () => Promise<T>, timeoutMilliseconds: number, timeoutMessage?: string): Promise<T> => {
     return new Promise<T>((resolve, reject) => {
 
-        let promiseActive = true;
-
         const timeout = setTimeout(() => {
-            if (!promiseActive) {
-                return;
-            }
-            promiseActive = false;
+
             const message = timeoutMessage || `Promise timeout hit: ${timeoutMilliseconds}`;
 
             reject(message);
@@ -15,18 +10,10 @@ export const promisePlus = <T>(promise: () => Promise<T>, timeoutMilliseconds: n
 
         promise()
             .then((result) => {
-                if (!promiseActive) {
-                    return;
-                }
-                promiseActive = false;
                 clearTimeout(timeout);
                 resolve(result);
             })
             .catch((error) => {
-                if (!promiseActive) {
-                    return;
-                }
-                promiseActive = false;
                 clearTimeout(timeout);
                 reject(error);
             });
