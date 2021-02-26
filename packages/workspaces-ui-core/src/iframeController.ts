b964f2f3d0a2424517f6f4cf3b381868cf67e375
Svetozar Mateev <svetozar.mateev@gmail.com>
2021-02-26 11:58:07 +0200

    Cleanup
    Signed-off-by: Svetozar Mateev <svetozar.mateev@gmail.com>

--- packages/workspaces-ui-core/src/iframeController.ts
+++ packages/workspaces-ui-core/src/iframeController.ts
@@ -133,34 +133,17 @@ export class IFrameController {
 
     public remove(id: string) {
         const frame = this._idToFrame[id];
-        console.log("FOUND frame to remove", id, frame);
         if (frame) {
-            console.log("DELETING frame from inner collections", id, frame);
             delete this._idToFrame[id];
-            console.log("WILL SEND post message", id, {
-                glue42core: {
-                    type: "manualUnload"
-                }
-            });
             frame.contentWindow.postMessage({
                 glue42core: {
                     type: "manualUnload"
                 }
             }, "*");
-            console.log("MESSAGE SENT", id, frame);
-            try {
-                frame.contentWindow.dispatchEvent(new Event("beforeunload"));
-            } catch (error) {
-                // tslint:disable-next-line: no-console
-                console.warn(error);
-            }
-            console.log("WILL REMOVE FRAME", id, frame);
             setImmediate(() => {
                 frame.remove();
-                console.log("FRAME REMOVED IN IMMEDIATE", id, frame);
             });
         }
-
     }
 
     public onFrameLoaded(callback: (frameId: string) => void) {
